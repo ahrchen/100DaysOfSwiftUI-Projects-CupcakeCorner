cbba6efbb5b2e2aab0f90d331a57b2e4e97d9779
Raymond Chen <ahrchen@gmail.com>
2022-03-05 21:46:40 -0800

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Raymond Chen on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
